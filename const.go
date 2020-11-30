3c777f37d2055aeed28eab7bbe819bcdeb39ccfa
jarrysix <jarrysix@gmail.com>
2020-11-30 12:17:32 +0800

    const.go

--- const.go
+++ const.go
@@ -5,6 +5,6 @@ const BuildVersion = "0.3.23"
 // 代码页
 const ReleaseCodeHome = "https://github.com/ixre/tto"
 // 发布时间
-const ReleaseDate = "2020-11-30 12:00"
+const ReleaseDate = "2020-11-30 12:15"
 // 兼容模式
 var CompactMode = false
